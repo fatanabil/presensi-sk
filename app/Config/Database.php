23a47f42a2829ba12faffee8bcfc09c7c884f17c
fatanabil <fatanabil63@gmail.com>
2021-11-02 13:27:44 +0700

    test deploy

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -50,26 +50,6 @@ class Database extends Config
 		'port'     => 5432,
 	];
 
-	// public $default = [
-	// 	'DSN'      => '',
-	// 	'hostname' => '127.0.0.1',
-	// 	'username' => 'root',
-	// 	'password' => '',
-	// 	'database' => 'absensi-siswa',
-	// 	'DBDriver' => 'MySQLi',
-	// 	'DBPrefix' => '',
-	// 	'pConnect' => false,
-	// 	'DBDebug'  => (ENVIRONMENT !== 'production'),
-	// 	'charset'  => 'utf8',
-	// 	'DBCollat' => 'utf8_general_ci',
-	// 	'swapPre'  => '',
-	// 	'encrypt'  => false,
-	// 	'compress' => false,
-	// 	'strictOn' => false,
-	// 	'failover' => [],
-	// 	'port'     => 3306,
-	// ];
-
 	//postgres
 	// public $default = [
 	// 	'DSN'      => '',
