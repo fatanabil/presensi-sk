7c1f43774397a2bbf7f75802317dff358d70a79f
fatanabil <fatanabil63@gmail.com>
2022-01-24 16:41:16 +0700

    change app base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'https://absensi-sk.herokuapp.com/';
+	public $baseURL = 'https://presensi-sk.herokuapp.com/';
 
 	/**
 	 * --------------------------------------------------------------------------
